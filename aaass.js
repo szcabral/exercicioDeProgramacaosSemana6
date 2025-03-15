d98cd4f51a74b87887570718fd3b6a17b2033dc7
Isabela Szolnoky Ferreira Cabral Souza da Conceição <isabela.conceicao@sou.inteli.edu.br>
2025-03-14 21:07:23 -0300

    Delete aaass.js

--- aaass.js
+++
@@ -1,9 +0,0 @@
-class Produto {
-    constructor (nome, preco){
-    this.nome = nome;
-    this.preco = preco;
-}
-    calcularDesconto () {
-        return this.preco - (this.preco * 0,10);
-    }
-}
